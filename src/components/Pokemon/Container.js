25ea85654aea45a61c37ec4151659b53cc1a7bde
Vinicius <vncs.coelho@gmail.com>
2018-12-17 23:58:21 -0200

    Created Container component, added to App.js and loaded main styles

---
+++ src/components/Pokemon/Container.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import Item from './Item';
+
+class Container extends Component {
+    state = {
+        pokemon: []
+    };
+
+    componentDidMount() {
+        this.getPokemonListFromAPI();
+    }
+
+    /* Get Pokemon from API and load into state */
+    getPokemonListFromAPI = async () => {
+        return await fetch(
+            'http://pokeapi.salestock.net/api/v2/pokemon/?limit=151'
+        )
+            .then(response => response.json())
+            .then(data => {
+                data.results.map((pokemon, key) => {
+                    /* Match IDs to their official "Entry in Pokedex" */
+                    key += 1;
+
+                    pokemon.id = key;
+
+                    pokemon.sprite =
+                        'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/' +
+                        key +
+                        '.png';
+
+                    return pokemon;
+                });
+
+                this.setState({
+                    pokemon: data.results
+                });
+            });
+    };
+
+    setPokemonList = pokemonList => {
+        console.log(pokemonList);
+        return pokemonList.map(item => <Item data={item} key={item.key} />);
+    };
+
+    render() {
+        return (
+            <div className="pokemon_container">
+                {this.setPokemonList(this.state.pokemon)}
+            </div>
+        );
+    }
+}
+
+export default Container;
