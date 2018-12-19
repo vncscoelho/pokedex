80d04486194b0f1a962263991fd461bc9b307c84
Vinicius <vncs.coelho@gmail.com>
2018-12-18 22:58:45 -0200

    Refactored styles and added ItemDetail component

--- src/components/Pokemon/Item.js
+++ src/components/Pokemon/Item.js
@@ -2,12 +2,26 @@ import React, { Component } from 'react';
 
 import { pad } from '../../Helper';
 
+import ItemDetail from './ItemDetail';
+
 class Item extends Component {
+    state = {
+        showDetail: true
+    };
+
+    toggleDetail = () => {
+        this.setState({ showDetail: !this.state.showDetail });
+    };
+
     render() {
         const data = { ...this.props.data };
         return (
-            <div className="item">
-                <div class="item_wrapper">
+            <div className="item" onClick={() => this.toggleDetail()}>
+                <div className="item_wrapper">
+                    {this.state.showDetail === true ? (
+                        <ItemDetail pokemonData={data} />
+                    ) : null}
+
                     <span className="item_entry">{pad(data.id)}</span>
                     <img
                         src={data.sprite}
---
+++ src/components/Pokemon/ItemDetail.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class ItemDetail extends Component {
+    render() {
+        return (
+            <div className="item-detail_wrapper">
+                <hr />
+            </div>
+        );
+    }
+}
+
+export default ItemDetail;
